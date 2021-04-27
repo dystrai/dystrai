4783dfb2767de0b6fd435f21396cd9158ab8e553
Jurandy Soares <jurandy.soares@gmail.com>
2021-04-27 08:15:06 -0300

    dysfaz: versão mais genérica

--- Scripts/dysfaz.sh
+++ Scripts/dysfaz.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
-# Dependências: git e meld
-# No Debian/Ubuntu: sudo apt install -y git meld
-
 # Passeio por todos os commits de um projeto
-# A fazer:
+
+# O diretório de trabalho é um diretório de trabalho do Git?
+# https://stackoverflow.com/questions/2180270/check-if-current-directory-is-a-git-repository
+# https://stackoverflow.com/questions/876239/how-to-redirect-and-append-both-stdout-and-stderr-to-a-file-with-bash
+if ! git rev-parse --is-inside-work-tree &> /dev/null; then
+    echo "O diretório atual não é um diretório de trabalho do Git."
+    exit 1
+fi
+
+
+# Um visualizador de diferenças está configurado?
 #    Verificar: git config --global diff.tool
-#    Sugerir: git config --global diff.tool meld
+#    Sugestão para Debian/Ubuntu: 
+#             sudo apt update
+#             sudo apt install -y meld
+#             git config --global diff.tool meld
 #             git config --global alias.dt 'difftool -d'
 # https://kparal.wordpress.com/2020/12/16/show-a-side-by-side-git-diff-on-any-commit-in-tig-using-meld/
 
+dt=$(git config --global diff.tool)
+# https://www.cyberciti.biz/faq/unix-linux-bash-script-check-if-variable-is-empty/
+if [ -z "$dt" ]; then
+    echo "Configure uma ferramenta para visualização de diferenças"
+    exit 1
+fi
+
+# https://opensource.com/article/18/5/you-dont-know-bash-intro-bash-arrays
 hashes=($(git log --reverse --pretty=oneline | awk '{print $1}'))
 
 hashes_len=${#hashes[@]}
