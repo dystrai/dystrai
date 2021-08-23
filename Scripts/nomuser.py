1cb0b33bce7a298cab2cb7fc3e979dd6eeeae11a
Jurandy Soares <jurandy.soares@gmail.com>
2021-08-23 05:59:43 -0300

    Cria nome de usuarios distintos

--- Scripts/nomuser.py
+++ Scripts/nomuser.py
@@ -8,11 +8,20 @@ import sys
 
 PREPS_DE_NOMES = ('de', 'da', 'das', 'do', 'dos', 'e')
 
+nome_para_login = {}
+
 def nomuser(nome_completo: str) -> str:
     nome_sobrenomes = [n.lower() for n in nome_completo.split() if n.lower() not in PREPS_DE_NOMES]
     if len(nome_sobrenomes) > 1:
         # Retorna {{nome}}.{{último sobrenome}}
-        return f'{nome_sobrenomes[0]}.{nome_sobrenomes[-1]}'
+        login = f'{nome_sobrenomes[0]}.{nome_sobrenomes[-1]}'
+        n = 0
+        while login in nome_para_login:
+            n += 1
+            login = f'{nome_sobrenomes[0]}.{nome_sobrenomes[-1]}{n}'
+
+        nome_para_login[login] = nome_sobrenomes
+        return login
     else:
         return nome_completo.lower()
 
