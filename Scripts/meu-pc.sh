1b371a923185786b0b3ed83e893fe9de031fc577
Jurandy Soares <jurandysoares@users.noreply.github.com>
2022-01-18 12:46:33 -0300

    Update meu-pc.sh

--- Scripts/meu-pc.sh
+++ Scripts/meu-pc.sh
@@ -2,14 +2,25 @@
 
 PC="pc-${USER}"
 CONT=$(lxc list -c n ${PC} --format=csv)
+ARG1="$1"
 
 if [ "${CONT}" == "" ]; then
-    lxc launch images:debian/11 $PC
+    lxc launch ubuntu:20.04 $PC
 fi
 
 figlet $(echo $PC | tr '[a-z]' '[A-Z]') | lolcat
 
-data=$(date +'%F %T')
+STATUS=$(lxc list -c s ${PC} --format=csv)
 
-lxc start $PC
-asciinema rec -i 2.5 -t "${PC}: ${data}" -c "lxc exec $PC bash"
+if [ "${STATUS}" == "STOPPED" ]; then
+    lxc start $PC
+fi
+
+if [ "${ARG1}" == "grava" ]; then
+  data=$(date +'%F %T')
+  asciinema rec -i 2.5 -t "${PC}: ${data}" -c "lxc exec $PC bash"
+else
+  lxc exec $PC bash
+fi
+
+# vim: ts=2 sw=2 et ai
