69f53a2f89cfba9cabc4c4039baf88ebfc914387
Jurandy Soares <jurandy.soares@gmail.com>
2022-07-26 16:00:24 -0300

    Melhorias na explicação dos comandos

--- Scripts/explcmd.py
+++ Scripts/explcmd.py
@@ -4,7 +4,15 @@
 explicacmd: Explica comando com Markdown
 """
 
+from pathlib import Path
 import sys
+
+try:
+    from slugify import slugify
+except:
+    print('Por favor, instale o módulo slugify!')
+    print('Execute: pip3 install python-slugify')
+    sys.exit(1)
 
 class Argumento:
     def __init__(self, arg: str, num: int) -> None:
@@ -19,28 +27,53 @@ class Argumento:
         self.num_centralizado = str(self.num).center(len(self.ornamento), ' ')
 
     def __str__(self) -> str:
-        return self.arg
+        if ' ' in self.arg:
+            return f'"{self.arg}"'
+        else:
+            return self.arg
 
 def main():
     
     if len(sys.argv) > 1:
+        slug = slugify(' '.join(sys.argv[1:]))
         args = [Argumento(arg, i) for i,arg in enumerate(sys.argv[1:], start=1)]
-        print('```')
-        print(' '.join([a.arg for a in args]))
-        print(' '.join([a.ornamento for a in args]))
-        print(' '.join([str(a.num).center(len(a.ornamento), ' ') for a in args]))
-        print('```')
-        print()
-        for a in args:
-            print(f'{a.num}. `{a.arg}`')
-            print()
-            print(f'{" "*(len(str(a.num))+2)} Explicação')
-            print()
 
-    else: # Ler da entrada padrão
-        while linha := sys.stdin.readline():
-	        quebrada = linha.split()
-        # TODO: The same as above, but taking into account the number shown at the beginning of line (history number?)
+        cam_arq_saida = Path() / 'saidas' / f'{slug}.txt'
+        with cam_arq_saida.open(mode='w', encoding='utf-8') as arq_saida:
+            pass
+
+        with open(f'{slug}.md', mode='w', encoding='utf-8') as arquivo_expl:
+            arquivo_expl.write(f'''\
+
+(comando:{slug})=
+
+# Comando `{' '.join([str(a) for a in args])}`
+
+
+''')            
+            arquivo_expl.write('```'+'\n')
+            arquivo_expl.write(' '.join([a.arg for a in args])+'\n')
+            arquivo_expl.write(' '.join([a.ornamento for a in args])+'\n')
+            arquivo_expl.write(' '.join([str(a.num).center(len(a.ornamento), ' ') for a in args])+'\n')
+            arquivo_expl.write('```'+'\n')
+            arquivo_expl.write('\n')
+            for a in args:
+                arquivo_expl.write(f'{a.num}. `{a.arg}`'+'\n')
+                arquivo_expl.write('\n')
+                arquivo_expl.write(f'{" "*(len(str(a.num))+2)} Explicação'+'\n')
+                arquivo_expl.write('\n')
+
+            arquivo_expl.write(f'''
+
+## Saída
+
+```{{literalinclude}} saidas/{slug}.txt
+:linenos:
+```
+''')
+
+
+    
 if __name__ == '__main__':
     main()
 
