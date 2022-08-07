83c5d4e48d0bf1b79bf2539d9cf3774d0950857b
Jurandy Soares <jurandy.soares@gmail.com>
2022-08-07 08:37:07 -0300

    Script que executa agente SSH com openssh

--- Scripts/agente-ssh.sh
+++ Scripts/agente-ssh.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+# TODO: Detect if script was sourced
+# Ref.: [bash - How to detect if a script is being sourced - Stack Overflow]
+# Link: https://stackoverflow.com/questions/2683279/how-to-detect-if-a-script-is-being-sourced
+# 
 
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
+
+
