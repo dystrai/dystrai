10dc544b2de7b56ee6f5a4a2fc6670c200e4e555
Jurandy Soares <jurandy.soares@gmail.com>
2022-08-08 16:31:08 -0300

    feat: ipirado com comparação binária

--- Scripts/ipirado.py
+++ Scripts/ipirado.py
@@ -1,39 +1,85 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import csv
 import ipaddress
+import re
 
-iface = ipaddress.ip_interface("188.184.7.219/27")
+pontuacao_para = {
+    'email': 0,
+    'end_iface': 0,
+    'end_rede': 0,
+    'end_broadcast': 0,
+    'qt_bits_redes': 0,
+    'qt_bits_hosts': 0,
+    'n_hosts_rede': 0,
+    'end_iface_bin': 0,
+    'end_iface_classe': 0,
+}
 
-type(iface)
-dir(iface)
+def classe_ipv4_de(ip_v4: str) -> str:
+    ip = [int(octeto) for octeto in ip_v4.split('.')]
 
-net = iface.network
-type(net)
-dir(net)
-print(net)
+    if (ip[0] >= 0 and ip[0] <= 127):
+        return "A"
+    
+    elif(ip[0] >=128 and ip[0] <= 191):
+        return "B"
+    
+    elif(ip[0] >= 192 and ip[0] <= 223):
+        return "C"
+    
+    elif(ip[0] >= 224 and ip[0] <= 239):
+        return "D"
+    
+    else:
+        return "E"
 
-len(list(net.hosts()))
 
-for host in net.hosts():
-    print(host)
+leitor_csv = csv.DictReader(open('av02-respostas.csv'))
+respostas_de = {linha['email']:linha for linha in leitor_csv}
 
-for i,host in enumerate(net.hosts(), start=1):
-    print(f'Host {i}: {host}')
+# Formata as respostas
+for email,respostas in respostas_de.items():
+    for resp in respostas:
+        respostas_de[email][resp] = respostas_de[email][resp].replace('"', '')
 
-print(net.broadcast_address)
+# respostas = {}
+# for linha in leitor_csv:
+#     respostas[linha['email']] = linha
 
-matricula = 2020987654321
-print(f'Minha matrícula é {matricula} na notação decimal.')
-print(f'Minha matrícula é {matricula:b} na notação binária.')
-print(f'Minha matrícula é {matricula:x} na notação hexadecimal.')
+expr_ipv4_bin = r"[0-1]{8}\.[0-1]{8}\.[0-1]{8}\.[0-1]{8}"
+padrao_ipv4_bin = re.compile(expr_ipv4_bin)
 
-octetos_dec = '188.184.7.223'.split(".")
-print(octetos_dec)
+def comp_enderecos_binarios(ender1: str, ender2: str) -> int:
+    assert (len(padrao_ipv4_bin.findall(ender1))==1) and len(padrao_ipv4_bin.findall(ender2))==1
+    
+    ender1_lst = list(ender1)
+    ender2_lst = list(ender2)
+    diff_end_1e2 = [bit_ender1==bit_ender2 for bit_ender1,bit_ender2 in zip(ender1_lst,ender2_lst)]
+    print(ender1, ender2, sep='\n')
+    print(''.join([' ' if valor else '|' for valor in diff_end_1e2]))
+    return diff_end_1e2.count(False)
 
-octectos_bin = [f'{int(x):08b}' for x in octetos_dec]
-print('.'.join(octectos_bin))
+def analisa_ip(ip_cidr: str):
+    ip,prefixo = ip_cidr.split('/')
+    iface = ipaddress.ip_interface(ip_cidr)
+    net = iface.network
 
-octs_bin = "10001110.01100000.01000111.10111010".split('.')
-octs_dec = [int(x, 2) for x in octs_bin]
-print('.'.join([str(y) for y in octs_dec]))
+    print(f'Rede: {net}')
+    print(f'Broadcast: {net.broadcast_address}/{prefixo}')
+    print(f'Número de dispositivos: {net.num_addresses-2}')
+    octetos_dec = ip.split(".")
+    octectos_bin = [f'{int(x):08b}' for x in octetos_dec]
+    end_iface_bin = '.'.join(octectos_bin)
+    print(f'Binário: {end_iface_bin}')
+    bin2compare = input('Binário para comparar: ')
+    print(end_iface_bin, bin2compare, sep='\n')
+    print('Iguais:', bin2compare==end_iface_bin)
+    comp_enderecos_binarios(end_iface_bin, bin2compare)
+    classe_ipv4 = classe_ipv4_de(ip)
+    print(f'Classe: {classe_ipv4}')
+
+
+while ip_cidr := input('Endereço IP na notação CIDR: '):
+    analisa_ip(ip_cidr)
