412104a3986736e57d8a6e698988abb6c04e04cf
Jurandy Soares <jurandy.soares@gmail.com>
2022-11-02 20:44:32 -0300

    feat: explcmd adiciona referência aos comandos

--- Scripts/explcmd.py
+++ Scripts/explcmd.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 
 """
-explicacmd: Explica comando com Markdown
+explcmd: Explica comando com Markdown
 """
 
+from os import environ as vars_amb
 from pathlib import Path
+import shlex
 import sys
+
+try:
+    from bs4 import BeautifulSoup
+except:
+    print('Por favor, instale o módulo bs4!')
+    print('Execute: pip3 install bs4')
+    sys.exit(1)
+
+
+try:
+    import requests
+except:
+    print('Por favor, instale o módulo requests!')
+    print('Execute: pip3 install requests')
+    sys.exit(1)
 
 try:
     from slugify import slugify
@@ -13,6 +30,30 @@ except:
     print('Por favor, instale o módulo slugify!')
     print('Execute: pip3 install python-slugify')
     sys.exit(1)
+
+
+paths = [Path(p) for p in vars_amb['PATH'].split(':')]
+
+def find_cmd_path(comando: str) -> Path:
+    for p in paths:
+       path = p/comando
+       if path.exists():
+           return path
+
+    return None     
+
+def md_link(url: str) -> str:
+    global resp
+    resp = requests.get(url)
+    
+    if resp.ok:
+        if resp.headers['Content-Type'].startswith('text/html;') \
+            or (resp.headers['Content-Type'] == 'text/html'):
+                
+                sopa = BeautifulSoup(resp.text, 'html.parser')
+                tag_titulo = sopa.find_all('title')[0]
+                titulo = tag_titulo.text
+                return f'[{titulo}]({url})'
 
 class Argumento:
     def __init__(self, arg: str, num: int) -> None:
@@ -33,17 +74,22 @@ class Argumento:
             return self.arg
 
 def main():
-    
+
     if len(sys.argv) > 1:
         slug = slugify(' '.join(sys.argv[1:]))
         args = [Argumento(arg, i) for i,arg in enumerate(sys.argv[1:], start=1)]
 
-        cam_arq_saida = Path() / 'saidas' / f'{slug}.txt'
+        cam_arq_md = Path() / 'comandos' / f'{slug}.md'
+        cam_arq_saida = cam_arq_md.parent / 'saidas' / f'{slug}.txt'
+
+        dir_name = cam_arq_saida.parent
+        dir_name.mkdir(mode=0o750, parents=True, exist_ok=True)
+
         with cam_arq_saida.open(mode='w', encoding='utf-8') as arq_saida:
             pass
 
-        with open(f'{slug}.md', mode='w', encoding='utf-8') as arquivo_expl:
-            arquivo_expl.write(f'''\
+        with cam_arq_md.open(mode='w', encoding='utf-8') as arq_md:
+            arq_md.write(f'''\
 
 (comando:{slug})=
 
@@ -51,19 +97,19 @@ def main():
 
 
 ''')            
-            arquivo_expl.write('```'+'\n')
-            arquivo_expl.write(' '.join([a.arg for a in args])+'\n')
-            arquivo_expl.write(' '.join([a.ornamento for a in args])+'\n')
-            arquivo_expl.write(' '.join([str(a.num).center(len(a.ornamento), ' ') for a in args])+'\n')
-            arquivo_expl.write('```'+'\n')
-            arquivo_expl.write('\n')
+            arq_md.write('```'+'\n')
+            arq_md.write(' '.join([a.arg for a in args])+'\n')
+            arq_md.write(' '.join([a.ornamento for a in args])+'\n')
+            arq_md.write(' '.join([str(a.num).center(len(a.ornamento), ' ') for a in args])+'\n')
+            arq_md.write('```'+'\n')
+            arq_md.write('\n')
             for a in args:
-                arquivo_expl.write(f'{a.num}. `{a.arg}`'+'\n')
-                arquivo_expl.write('\n')
-                arquivo_expl.write(f'{" "*(len(str(a.num))+2)} Explicação'+'\n')
-                arquivo_expl.write('\n')
+                arq_md.write(f'{a.num}. `{a.arg}`'+'\n')
+                arq_md.write('\n')
+                arq_md.write(f'{" "*(len(str(a.num))+2)} Explicação'+'\n')
+                arq_md.write('\n')
 
-            arquivo_expl.write(f'''
+            arq_md.write(f'''
 
 ## Saída
 
@@ -72,7 +118,27 @@ def main():
 ```
 ''')
 
+            arq_md.write(f'''\
 
+## Referências
+
+''')
+            
+            # TODO: Detectar se é um comando interno do shell (bash ou zsh)
+            # Ver: Linux / Unix Bash Shell List All Builtin Commands
+            #      <https://www.cyberciti.biz/faq/linux-unix-bash-shell-list-all-builtin-commands/>
+            
+            cmd = sys.argv[1]
+            urls = [
+                f'https://guialinux.uniriotec.br/{cmd}/',
+                f'https://man7.org/linux/man-pages/man1/{cmd}.1.html',
+            ]
+            
+            for url in urls:
+                link = md_link(url)
+                print(url, link)
+                if link:    
+                    arq_md.write(f'- {link}\n')            
     
 if __name__ == '__main__':
     main()
